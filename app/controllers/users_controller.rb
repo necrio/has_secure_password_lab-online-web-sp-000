9e407a1d2f6bd5c67fa43bf9904358c3f9b8c7b0
Keith Isham <kisham8899@gmail.com>
2020-02-17 19:07:11 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    return redirect_to '/users/new' if params[:user][:password] != params[:user][:password_confirmation]
+    User.new(user_params).save
+    session[:user_id] = User.last.id
+    redirect_to '/'
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
