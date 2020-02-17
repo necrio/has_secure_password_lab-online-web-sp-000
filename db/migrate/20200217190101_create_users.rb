22e78ac661dd7811def7a3ea5d9221ae557d6de5
Keith Isham <kisham8899@gmail.com>
2020-02-17 19:05:33 +0000

    Automatically backed up by Learn

--- db/migrate/20200217190101_create_users.rb
+++ db/migrate/20200217190101_create_users.rb
@@ -4,9 +4,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :username
       t.string :password_digest
 
-    t.timestamps null: false
+      t.timestamps null: false
 
-      t.timestamps
+
     end
   end
 end
